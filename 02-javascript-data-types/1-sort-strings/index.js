b8f2f4cd7ea3d7afc63deb2b6ea0c6a2e9b52308
Serg1177 <98102615+Serg1177@users.noreply.github.com>
2024-01-21 09:14:27 +0300

    02-task- index.js

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+     if(param == 'asc') { 
+     return arr.sort((a, b) => a.localeCompare(b))
+      // return arr.sort()
+     }else{
+     return arr.sort((a, b) => a.localeCompare(b)).reverse()
+    }
+   }
 }
