8e47624ab1259a97bab6110f8afb73f8e6723af6
Serg1177 <riziden@yandex.ru>
2024-01-22 15:23:32 +0300

    tren_1

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,9 +5,7 @@
  * @param {number} n second number
  * @returns {number}
  */
-<<<<<<< HEAD
+
 export const sum = (m, n) => m + n;
-=======
-export const sum = (m, n) => m+n;
-sum()
->>>>>>> 3ad14b5e14a1f3791723bbd425adceaba22db255
+
+
