9f772a4c2c08a46171aab147969c51fe0a7d0e20
Serg1177 <riziden@yandex.ru>
2024-01-25 13:28:45 +0300

    Tascs_3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,19 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
+ 
+
 export function createGetter(path) {
 
+  return function(obj) {
+    let parts = path.split(".");
+    let current = obj;
+    for (let i = 0; i < parts.length; i++) {
+      current = current[parts[i]];
+      if (!current)
+      {break;}
+    }
+    return current;
+  };
+  
 }
