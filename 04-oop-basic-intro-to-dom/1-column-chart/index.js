e072e6e578953c2555eb600ea6d513fd26407fed
Serg1177 <riziden@yandex.ru>
2024-01-31 22:13:41 +0300

    4_1-component_1_1

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,38 @@
 export default class ColumnChart {
 
+  constructor({data, label, value, link}) {
+    this.data = data;
+    this.label = label;
+
+    this.value = value;
+    this.link = link;
+    this.element = this.element();
+
+  }
+  element() {
+  //создаем блоки
+ 
+    let columnChart = document.createElement('div');
+    let nameChart = document.createElement('div');
+    let columnAmount = document.createElement('div');
+
+
+    nameChart.innerHTML += `${this.label}`;
+    columnAmount.innerHTML += `${this.value}`;
+  
+    //nameChart.append(<p>this.label</p>)
+    columnChart.className = "column-chart__chart";
+    nameChart.className = 'column-chart__title';
+    columnAmount.className = 'column-chart__header';
+
+    columnChart.append(nameChart);
+    columnChart.append(columnAmount);
+  
+    console.log(columnChart);
+    
+  
+
+    return columnChart;
+  }
+
 }
