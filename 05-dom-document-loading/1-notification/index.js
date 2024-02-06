b0895060dfa76e28457976f4b874ed7a3ad18179
Serg1177 <riziden@yandex.ru>
2024-02-06 14:43:30 +0300

    task 5_1 test_1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,45 @@
+
+
 export default class NotificationMessage {
+    element;
+    
+    constructor(
+      message = '',
+      {
+        duration = 0,
+        type = ''
+      } = {}) {
+      this.message = message;
+      this.duration = duration;
+      this.type = type;
+      
+      this.element = this.createElement(this.createTemplate())
+    }
+  
+    createElement(template) {
+      const element = document.createElement('div');
+      element.innerHTML = template;
+
+      document.body.append(element.firstElementChild)
+      //console.log(element.firstElementChild)
+      return element.firstElementChild;
+    }
+
+    createTemplate() {
+      return `  
+      <div class="notification success" style="--value:20s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">success</div>
+      <div class="notification-body">
+        Hello world
+      </div>
+    </div>
+  </div>
+      `;   
+    }
+    show() {
+      document.body.append(this.element.firstElementChild)
+    }
 
 }
