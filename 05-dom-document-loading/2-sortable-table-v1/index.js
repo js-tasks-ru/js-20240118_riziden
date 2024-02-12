7e50ab57c076830708619faf4f04b928024b277e
Serg1177 <riziden@yandex.ru>
2024-02-12 21:53:33 +0300

    task 5-2_testsAll

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,127 +1,119 @@
 
-
 export default class SortableTable {
   static lastTable;
+  
   
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
     this.data = data;
-
-    this.element = this.createElement(this.createTemplate());
+    this.subElements = {};
+    this.element = this.createElement(this.createTemplate()); 
   }
 
   createElement(template) {
-    //this.show()
-    
     
     const element = document.createElement('div');
     element.innerHTML = template;
-    //console.log(element)
-    //document.body.appendChild(element)
     this.lastTable = element;
-    return element;
+    return element.firstElementChild;
   }
 
   createTemplate() {
-    const head = ` 
+    return ` 
   <div  data-element="productsContainer" class="products-list__container">
   <div class="sortable-table">
 
     <div data-element="header" class="sortable-table__header sortable-table__row">
-      <div class="sortable-table__cell" data-id="images" data-sortable="false" data-order="asc">
-        <span>Image</span>
-      </div>
-      <div class="sortable-table__cell" data-id="title" data-sortable="true" data-order="asc">
-        <span>Name</span>
-        <span data-element="arrow" class="sortable-table__sort-arrow">
-          <span class="sort-arrow"></span>
-        </span>
-      </div>
-      <div class="sortable-table__cell" data-id="quantity" data-sortable="true" data-order="asc" dataset = "order">
-        <span>Quantity</span>
-      </div>
-      <div class="sortable-table__cell" data-id="price" data-sortable="true" data-order="asc">
-        <span>Price</span>
-      </div>
-      <div class="sortable-table__cell" data-id="sales" data-sortable="true" data-order="asc">
-        <span>Sales</span>
-      </div>
+    ${this.createTemplateHeader()}
     </div> 
     <div id = "glav" data-element="body" class="sortable-table__body">
         ${this.createTableBody()}
       </div>
   </div>
   </div>
-    `;
-    return head;   
+    `; 
+  }
+
+  createTemplateHeader() {
+    return this.headerConfig.map((i)=>{         
+      return `
+      <div class="sortable-table__cell" data-id=${i.id} data-sortable="false">
+        <span>${i.title}</span>
+      </div>
+    `
+    
+    }).join('') ;
   }
 
   createTableBody() {
     let tableRow = '';
 
     const tableBody = this.data.forEach((elem)=>{
-      // sort
-
-      tableRow += this.createTableRow(elem.title, elem.quantity, elem.price, elem.sales)
-
+      
+      tableRow += this.createTableRow(elem);
+      
     });
     
     return tableRow;
   }
 
-  createTableRow(title, quantity, price, sales) {
-
-  
+  createTableRow(elem) {
     return `
-    
     <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
-        <div class="sortable-table__cell">
-          <img class="sortable-table-image" alt="Image" src="http://magazilla.ru/jpg_zoom1/246743.jpg">
-        </div>
-        <div class="sortable-table__cell">${title}</div>
-
-        <div class="sortable-table__cell">${quantity}</div>
-        <div class="sortable-table__cell">${price}</div>
-        <div class="sortable-table__cell">${sales}</div>
+        ${this.createPartRow(elem)}
       </a>
-      </div>
     `
   }
 
-  sort(field, order) {
-    
-    // let dataCopy = [...this.data];
-    if (order) {
-      this.element.remove()
-      this.data.sort((a, b)=>{
-        return a[`${field}`] - b[`${field}`];
-        //return a.price - b.price;
-      });
-
-      
-    }
-    console.log(this.data)
-    this.element = this.createElement(this.createTemplate());
-    let root = document.getElementById("root")
-    root.append(this.element);
-
-    //return this.data;
-    
+  createPartRow(elem) {
+    return this.headerConfig.map((i)=>{ 
+      if (i.id == 'images') {
+        return `
+      <div class="sortable-table__cell"></div>
+    `
+      }      
+      return `
+      <div class="sortable-table__cell">${elem[i.id]}</div>
+    `
+   }).join('') ;
   }
 
-  /* show() {
-     if (SortableTable.lastTable) {
-      SortableTable.lastTable.remove();
-    //hide
-    }
-    //show
-    let pro = document.getElementById("glav");
-    let root = document.getElementById("root")
-    root.append(this.element);
-    SortableTable.lastTable = this.element; 
-   
-  } */
+  sort(field, order) {
+    let sortType = 'string';
+    let copyData;
+    this.headerConfig.map((i)=>{
+      if(i.id == field) sortType = i.sortType;
+    });
 
+    this.element.remove();
+
+    if (order == 'asc') {
+      copyData = this.data.sort((a, b)=>{
+        if (sortType == 'string') {  
+         
+          return a[field].localeCompare(b[field], 'ru', { caseFirst: 'upper' });
+        }
+        return a[field] - b[field];
+      }); 
+    } else {
+      copyData = this.data.sort((a, b)=>{
+        if (sortType == 'string') {  
+          return b[`${field}`].localeCompare(a[`${field}`], 'ru', { caseFirst: 'upper' });
+        }
+        return b[`${field}`] - a[`${field}`];
+      }); 
+    }
+
+    
+    this.element = this.createElement(this.createTemplate(copyData));
+    document.body.append(this.element)
+   
+    this.element.querySelectorAll('[data-element]').forEach(element => {
+      this.subElements[element.dataset.element] = element; 
+    });
+      
+    this.subElements.body.innerHTML = this.createTableBody();
+  }
 
   destroy() {
     this.element.remove();
