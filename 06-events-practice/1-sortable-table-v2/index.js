ac80fc82dc6739209ba0ed7ab20f01ad974bf53f
Serg1177 <riziden@yandex.ru>
2024-03-05 16:26:03 +0300

    6_1-try4

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -16,21 +16,15 @@ export default class SortableTable extends SortableTableV1 {
     this.data = data;
     this.sorted = sorted;
     this.element = this.createElement(super.createTemplate()); 
-    this.selectSubElements();
+    this.selectSubElements = super.selectSubElements();
     this.sort(this.sorted.id, this.sorted.order);
-    
+    this.element.addEventListener('pointerdown', this.onDivClick);
   }
-
-  
 
 
   createElement(template) {
-   
     const element = document.createElement('div');
     element.innerHTML = template;
-
-    element.firstElementChild.addEventListener('pointerdown', this.onDivClick);
-    
     return element.firstElementChild;
   }
 
@@ -39,91 +33,13 @@ export default class SortableTable extends SortableTableV1 {
       let el = e.target.closest('[data-id]');
       this.sort(el.dataset.id, el.dataset.order);
 
-      console.log(el.dataset.id, el.dataset.order);
-
       if (el.dataset.order == "desc") {el.dataset.order = "asc";
       } else {
         el.dataset.order = "desc";}
     }
   }
-
-  selectSubElements() {
-    this.element.querySelectorAll('[data-element]').forEach(element => {
-      this.subElements[element.dataset.element] = element;
-      //this.createElementsHandler(element);
-    });
-   
-  }
-  
-  /* createElementsHandler(element) {
-    element.querySelectorAll('[data-sortable]').forEach(elem => {
-
-      elem.addEventListener('pointerdown', ()=>{this.sort(elem.dataset.id, elem.dataset.order);
-        (elem.dataset.order == "desc") ? elem.dataset.order = "asc" : elem.dataset.order = "desc";
-      });
-      
-    });
-  }  */
-
-
-
-  
-  /* createTemplate() {
-    return ` 
-  <div  data-element="productsContainer" class="products-list__container">
-  <div class="sortable-table">
-
-    <div data-element="header" class="sortable-table__header sortable-table__row">
-    ${super.createHeaderTemplate()}
-    </div> 
-    <div data-element="body" class="sortable-table__body">
-    ${super.createBodyTemplate()}
-      </div>
-  </div>
-  </div>
-    `; 
-  } */
-
-  /* createHeaderTemplate() { 
-    return this.headersConfig.map((element)=>{         
-      return `
-      <div  class="sortable-table__cell" data-id=${element.id} data-sortable=${element.sortable} data-order = "asc">
-        <span>${element.title}</span>
-      </div>
-    `
-    }).join('') ;
-  } */
-
-  /* createBodyTemplate() {
-    return this.data.map(this.createRowTemplate).join('');
-  }
-
-  createRowTemplate = (data) => {
-    return `
-      <a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
-        ${this.createRowContentTemplate(data)}
-      </a>
-    `;
-  }
-
-  createRowContentTemplate = (data) => {
-    return this.headersConfig.map((columnConfig)=>{ 
-      if (columnConfig.template) {
-        return columnConfig.template(data);
-      }
-
-      const fieldName = columnConfig.id;
-      const fieldValue = data[fieldName];
-  
-      return `<div class="sortable-table__cell" data-id=${fieldName}>
-      ${fieldValue}
-      <span data-element="arrow" class="sortable-table__sort-arrow">
-      </div>`;
-    }).join('') ;
-  }
- */
+ 
   sort(field = 'title', order = 'desc') {
-    //this.proba()
     const orders = {
       'desc': -1,
       'asc': 1,
